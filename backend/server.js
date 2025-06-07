65d1d06db7a7d5eddc9e8571102589296847780a
Ruslan Moryakov <ruslikmoryakov@gmail.com>
2025-06-07 16:34:35 +0300

    add controllers, routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './src/config/db.js'
+import router from './src/routes/user.route.js'
 
 connectDB()
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`)
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,15 +1,23 @@
-import express from 'express'
 import UserRegister from '../models/UserRegister.js'
 
 
 const getRegister = (req, res) => {
     try {
+        res.send('get response')
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+const postRegister = (req, res) => {
+    try {
         const {username, password} = req.body;
-        const register = new UserRegister({username, password}).save()
+        const register = new UserRegister({username, password})
+        register.save()
         .then((result) => {res.send(result)})
     } catch (error) {
         res.send(res.json({message: 'error'}))
     }
 }
 
-export {getRegister}
+export {getRegister, postRegister}
